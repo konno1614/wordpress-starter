35de4dc6a1a6abee07520ed315c9d11a227b7f72
Hirotoshi Konno <konno@buddying.jp>
2020-02-07 11:13:45 +0900

    不要テンプレート削除

--- public/wp-content/themes/shousei/footer.php
+++ public/wp-content/themes/shousei/footer.php
@@ -14,35 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="footer" class="o-footer">
-		<div class="o-footer__inner">
-			<?php get_template_part('template-parts/nav/pc-nav') ?>
-			<div class="o-footer__flex">
-				<div class="o-footer__logo">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?= get_theme_file_uri('img/header/logo_vertical.png'); ?>" alt="将星国際特許事務所"></a>
-				</div>
-				<ul class="o-footer__sns-list">
-					<li class="o-footer__sns-item">
-						<a href="https://www.facebook.com/shousei.pat/" target="_blank"><img src="<?= get_theme_file_uri('img/footer/icon-facebook.png'); ?>" alt="facebook"></a>
-					</li>
-					<li class="o-footer__sns-item">
-						<a href="https://www.instagram.com/shousei_pat/" target="_blank"><img src="<?= get_theme_file_uri('img/footer/icon-instagram.png'); ?>" alt="instagram"></a>
-					</li>
-				</ul>
-				<ul class="o-footer__link-list">
-					<li class="o-footer__link-item">
-						<a href="<?php echo esc_url( home_url( '/privacy-policy' ) ); ?>">
-							プライバシーポリシー
-						</a>
-					</li>
-					<li class="o-footer__link-item">
-						<a href="<?php echo esc_url( home_url( '/license-policy' ) ); ?>">
-							ライセンスポリシー
-						</a>
-					</li>
-				</ul>
-			</div>
-			<p class="o-footer__copyright">&copy; 2019 将星国際特許事務所</p>
-		</div>
+		<p>フッターです。</p>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- public/wp-content/themes/shousei/header.php
+++ public/wp-content/themes/shousei/header.php
@@ -13,29 +13,11 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-8618428-5"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-		gtag('config', 'UA-8618428-5');
-	</script>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/css/swiper.min.css">
 	<link rel="shortcut icon" href="<?= get_theme_file_uri('img/favicon.ico'); ?>">
-	<script>
-		(function(d) {
-			var config = {
-			kitId: 'hbs8ufv',
-			scriptTimeout: 3000,
-			async: true
-			},
-			h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-		})(document);
-	</script>
 	<?php wp_head(); ?>
 </head>
 
@@ -53,13 +35,7 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'shousei' ); ?></a>
 
 	<header id="masthead" class="o-header">
-		<div class="o-header__inner">
-			<h1 class="o-header__logo">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?= get_theme_file_uri('img/header/logo_landscape.png'); ?>" alt="将星国際特許事務所"></a>
-			</h1>
-			<?php get_template_part('template-parts/nav/pc-nav') ?>
-			<?php get_template_part('template-parts/nav/sp-nav') ?>
-		</div>
+		<p>ヘッダーです。</p>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
