4d2160f79f1f662a589de2dde733596c5ffad897
Hirotoshi Konno <konno@buddying.jp>
2020-02-10 14:12:56 +0900

    format:js-files

--- public/wp-content/themes/wordpress_starter/assets-src/js/lib/Mobile-Nav.js
+++ public/wp-content/themes/wordpress_starter/assets-src/js/lib/Mobile-Nav.js
@@ -1,11 +1,11 @@
 import $ from 'jquery'
 $(function () {
-	/**
-	 * spナビゲーション
-	 */
-	$('.js-toggle-nav').on('click', function () {
-		$('.js-toggle-nav').toggleClass('is_active')
-		$('.o-side-bar__hidden').toggleClass('is_active')
-		$('.o-nav-sp').toggleClass('is_active')
-	})
+  /**
+   * spナビゲーション
+   */
+  $('.js-toggle-nav').on('click', function () {
+    $('.js-toggle-nav').toggleClass('is_active')
+    $('.o-side-bar__hidden').toggleClass('is_active')
+    $('.o-nav-sp').toggleClass('is_active')
+  })
 })
